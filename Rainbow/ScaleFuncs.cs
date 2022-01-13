87f3b4c842607da6b36b230dea48f531831299fe
Makeloft <makeman@tut.by>
2022-01-13 22:10:19 +0300

    MAKE !

--- Rainbow/ScaleFuncs.cs
+++ Rainbow/ScaleFuncs.cs
@@ -7,28 +7,43 @@ namespace Rainbow
 		public static double Lineal(double value) => value;
 
 		public static double _20Log10(double value) =>
-			value > 0d ? +20d * Math.Log(+value * ushort.MaxValue, 10d) :
-			value < 0d ? -20d * Math.Log(-value * ushort.MaxValue, 10d) :
-			0d;
+			value > 0d ? +Math.Log(+value * ushort.MaxValue, 10d) * 20d :
+			value < 0d ? -Math.Log(-value * ushort.MaxValue, 10d) * 20d :
+			value;
+
+		public static double _10Pow20(double value) =>
+			value > 0d ? +Math.Pow(+value * ushort.MaxValue, 20d) / 10d :
+			value < 0d ? -Math.Pow(-value * ushort.MaxValue, 20d) / 10d :
+			value;
 
 		public static double Log2(double value) =>
 			value > 0d ? +Math.Log(+value, 2d) :
 			value < 0d ? -Math.Log(-value, 2d) :
-			0d;
+			value;
+
+		public static double _2Pow(double value) =>
+			value > 0d ? +Math.Pow(2, +value) :
+			value < 0d ? -Math.Pow(2, -value) :
+			value;
 
 		public static double Log(double value) =>
 			value > 0d ? +Math.Log(+value) :
 			value < 0d ? -Math.Log(-value) :
-			0d;
+			value;
 
 		public static double Exp(double value) =>
 			value > 0d ? +Math.Exp(+value) :
 			value < 0d ? -Math.Exp(-value) :
-			0d;
+			value;
 
 		public static double Sqrt(double value) =>
 			value > 0d ? +Math.Sqrt(+value) :
 			value < 0d ? -Math.Sqrt(-value) :
-			0d;
+			value;
+
+		public static double Pow2(double value) =>
+			value > 0d ? +value * value :
+			value < 0d ? -value * value :
+			value;
 	}
 }
