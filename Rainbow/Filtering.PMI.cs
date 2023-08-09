1a7eba794c20fdd2aa53c7c3d7ef302c7c28c9c0
Makeloft <makeman@tut.by>
2023-08-09 22:39:15 +0300

    MAKE !

--- Rainbow/Filtering.PMI.cs
+++ Rainbow/Filtering.PMI.cs
@@ -9,7 +9,7 @@ namespace Rainbow
 		public static List<Bin> Interpolate(this IList<Bin> spectrum, out List<Bin> peaks) =>
 			spectrum.Interpolate(peaks = new()).ToList();
 
-		public static bool TryReconstructPeak(this IList<Bin> spectrum, int i, out Bin peak, out double deltaPhase)
+		public static bool TryReconstructPeak(this IList<Bin> spectrum, int i, out Bin peak)
 		{
 			/* Frequency (F); Magnitude (M); Phase (P); */
 			spectrum[i + 0].Deconstruct(out var aF, out var aM, out var aP);
@@ -17,9 +17,22 @@ namespace Rainbow
 			spectrum[i + 2].Deconstruct(out var cF, out var cM, out var cP);
 			spectrum[i + 3].Deconstruct(out var dF, out var dM, out var dP);
 
+			var distance = (cP - bP) / Pi.Single;
+			var turn = Math.Abs(distance + 0.5) > 1.0;
+			if (turn && distance < -1.0)
+				distance += 2.0;
+
+			var isPeakByPhase = 0.9 < Math.Abs(distance) && Math.Abs(distance) <= 1.1;
+
+			if (isPeakByPhase is false)
+			{
+				peak = default;
+				return false;
+			}
+
 			//var halfStepF = (cF - bF) / 2;
 			//var bcMiddleF = (bF + cF) / 2;
-			var bcOffsetScale = (cM - bM) / (cM + bM);
+			//var bcOffsetScale = (cM - bM) / (cM + bM);
 			//var bcF = bcMiddleF + bcOffsetScale * halfStepF;
 
 			var bcF = (bF * bM + cF * cM) / (bM + cM);
@@ -27,62 +40,57 @@ namespace Rainbow
 			var bcP = bP + (bcF - bF) * (cP - bP) / (cF - bF);
 			/* y(x) = y0 + ( x  - x0) * (y1 - y0) / (x1 - x0) */
 
-			if (cP > bP)
+			if (turn)
 				bcP += Pi.Double * (cF - bcF) / (cF - bF);
 			if (bcP > Pi.Single)
 				bcP -= Pi.Double;
+			
+			//if (bcM < (bM + cM)) // resonanse estimation
+			//	bcM = Math.Sqrt(bM * bM + cM * cM);
+
+			//var _bcM = bM + (bcF - bF) * (cM - bM) / (cF - bF);
+			//var isPeakByMagnitude = _bcM < bcM;
+			var isPeakByMagnitude = bcM > 0d && bcM * bcM > bM * bM + cM * cM;
+			if (isPeakByMagnitude is false && bcM < (bM + cM)) // resonanse estimation
+				bcM = Math.Sqrt(bM * bM + cM * cM);
 
 			peak = new(bcF, bcM, bcP);
-			
-			deltaPhase = cP - bP;
 
-			var isPeakByFrequency = bcF.BelongOpen(bF, cF);
-			var isPeakByMagnitude = bcM > Math.Max(bM, cM) / 2d;
-			var isPeakByPhase = Math.Abs(deltaPhase).BelongClose((1d - Math.Abs(bcOffsetScale)) * Pi.Half, 1.5 * Pi.Single);
-			
-			return isPeakByPhase && isPeakByMagnitude && isPeakByFrequency;
+			return isPeakByPhase;
 		}
 
-
-		public static IEnumerable<Bin> Interpolate(this IList<Bin> spectrum, List<Bin> peaks)
+		public static IEnumerable<Bin> Interpolate(this IList<Bin> spectrum)
 		{
 			var count = spectrum.Count / 2;
 
 			for (var i = 0; i < count; i++)
 			{
-				var hasL = spectrum.TryReconstructPeak(i + 0, out var peakCandidateL, out var deltaPhaseL);
-				var hasR = spectrum.TryReconstructPeak(i + 1, out var peakCandidateR, out var deltaPhaseR);
-				
-				if (hasL is false || (hasR is true && Math.Abs(deltaPhaseR) > Math.Abs(deltaPhaseL)))
+				var hasPeak = spectrum.TryReconstructPeak(i + 0, out var peak);
+
+				yield return spectrum[i + 0];
+
+				if (hasPeak)
+					yield return peak;
+			}
+		}
+
+		public static IEnumerable<Bin> Interpolate(this IList<Bin> spectrum, List<Bin> peaks)
+		{
+			var interpolatedSpectrum = Interpolate(spectrum).OrderBy(p => p.Frequency).ToList();
+
+			for (var i = 1; i < interpolatedSpectrum.Count - 1; i++)
+			{
+				var a = interpolatedSpectrum[i - 1].Magnitude;
+				var b = interpolatedSpectrum[i].Magnitude;
+				var c = interpolatedSpectrum[i + 1].Magnitude;
+
+				if (b > Math.Max(a, c))
 				{
-					yield return spectrum[i];
-				}
-				else
-				{
-					peaks.Add(peakCandidateL);
-
-					var bcF = peakCandidateL.Frequency;
-					var bF = spectrum[i + 1].Frequency;
-					var cF = spectrum[i + 2].Frequency;
-
-					spectrum[i + 0].Deconstruct(out var aF, out var aM, out var aP);
-					spectrum[i + 3].Deconstruct(out var dF, out var dM, out var dP);
-
-					var abF = aF + (bcF - bF);
-					var abM = aM;
-					var abP = aP;
-
-					var dcF = dF + (bcF - cF);
-					var dcM = dM;
-					var dcP = dP;
-
-					yield return new(in abF, in abM, in abP);
-					yield return peakCandidateL;
-					yield return new(in dcF, in dcM, in dcP);
-
-					i += hasR ? 2 : 3;
+					peaks.Add(interpolatedSpectrum[i]);
 				}
 			}
+
+			return interpolatedSpectrum;
 		}
 	}
 }
